5956b158692dcc42a531d4e6bb955876412a7a60
e <569188901@qq.com>
2019-05-10 14:03:55 +0800

    mybatis注解增强版

---
+++ src/main/java/com/wsc/MybatisAnnotationNewApplication.java
@@ -0,0 +1,13 @@
+package com.wsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisAnnotationNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisAnnotationNewApplication.class, args);
+    }
+
+}
